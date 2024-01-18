2c144ba4f584946bcc1cb5e6bb76e86151ff3861
Estrella0812 <88253146+Estrella0812@users.noreply.github.com>
2024-01-18 17:38:32 -0500

    resume upload

--- src/pages/mainPageContent/Resume/Resume.js
+++ src/pages/mainPageContent/Resume/Resume.js
@@ -5,7 +5,7 @@ function Resume(){
     return(
         <div className="resume-container">
             <h2 className="resume-header">Resume</h2>
-            <a className="resume-download d-flex" href="resume.pdf" download="resume.pdf"><Download></Download><p>download</p></a>
+            <a className="resume-download d-flex" href="HanbyulKim_Resume.pdf" download="HanbyulKim_Resume.pdf"><Download></Download><p>download</p></a>
         </div>
     )
 }
