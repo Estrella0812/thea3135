ff9b5aac827987e0dba5a1a06860c2493bbdc804
Estrella0812 <88253146+Estrella0812@users.noreply.github.com>
2024-03-17 21:35:32 -0400

    module 17 and program project

--- src/pages/mainPageContent/Work/Work.js
+++ src/pages/mainPageContent/Work/Work.js
@@ -1,4 +1,4 @@
-import {Lock} from 'react-bootstrap-icons'
+import {Lock, Download, Link} from 'react-bootstrap-icons'
 import { useEffect, useState } from "react"
 import './Work.css'
 
@@ -8,6 +8,7 @@ import module15IMG2 from './img/photoshoped_image.jpg'
 import module16IMG1 from './img/module16_original.jpg'
 import module16IMG2 from './img/module16_mine.jpg'
 import module16IMG3 from './img/module16_AI.jpg'
+import module17IMG from './img/module17.png'
 
 function Work(){
     const [module, setModule] = useState([]);
@@ -72,7 +73,8 @@ function Work(){
             setModule([
                 <div className="work-container">
                 <h2 className="imageHeader">Original Image</h2>
-                <img className="module-img" src={module15IMG1} style={{width: "100%", marginBottom: "10vh"}}/>
+                <img className="module-img" src={module15IMG1} style={{width: "100%"}}/>
+                <a href=" https://www.youtube.com/watch?app=desktop&v=OFrgwlbcZ1w" target="_blank" style={{marginBottom: "10vh"}}>original image by Billboard</a>
                 <h2 className="imageHeader">Photoshopped Image</h2>
                 <img className="module-img" src={module15IMG2} style={{width: "100%"}}/>
                 <button className="work-button" onClick={setModuleCat}>back</button>
@@ -130,6 +132,32 @@ function Work(){
             </div>
             ]);
         }
+        if(e.target.innerText==="Module 17"){
+            setModule([
+                <div className="work-container">
+                    <h2 className="imageHeader" style={{marginTop: "10vh"}}>Lost Pet Flyer</h2>
+                    <img className="module-img" src={module17IMG} style={{width: "500px", marginBottom: "10vh"}}/>
+                    <button className="work-button" onClick={setModuleCat}>back</button>
+                </div>
+            ])
+        }
+        if(e.target.innerText==="Program Project"){
+            setModule([
+                <div className="work-container">
+                    <div className="work-cont-container" style={{minHeight: "70vh", marginTop: "20vh"}}>
+                        <h2 className="resume-header">Program Project pdf</h2>
+                        <section className="work-written-section">
+                            <a className="resume-download d-flex" href="Program Project.pdf" download="Program Project.pdf" style={{minWidth: "180px", marginTop: "30px", marginBottom: "13vh"}}><Download></Download><p>download</p></a>
+                        </section>
+                        <h2 className="resume-header">Link</h2>
+                        <section className="work-written-section">
+                            <a className="resume-download d-flex" href="https://www.canva.com/design/DAF_z_8jf-w/Na2a01zAm0ahDJCsOdXo6Q/edit?utm_content=DAF_z_8jf-w&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank" style={{minWidth: "180px"}}><Link></Link><p>Visit Canva</p></a>
+                        </section>
+                    </div>
+                    <button className="work-button" onClick={setModuleCat}>back</button>
+                </div>
+            ])
+        }
         if(e.target.innerText==="back"){
             setModule(workButtons);
         }
@@ -137,12 +165,12 @@ function Work(){
 
     const workButtons = [
         <div className="Work-button-container">
+            <button className="work-button" onClick={setModuleCat}>Program Project</button>
             <button className="work-button" onClick={setModuleCat}>Module 12</button>
             <button className="work-button" onClick={setModuleCat}>Module 14 </button>
             <button className="work-button" onClick={setModuleCat}>Module 15</button>
             <button className="work-button" onClick={setModuleCat}>Module 16</button>
-            <button className="work-button">Module 17 <Lock></Lock></button>
-            <button className="work-button">Module 18 <Lock></Lock></button>
+            <button className="work-button" onClick={setModuleCat}>Module 17</button>
             <button className="work-button">Module 19 <Lock></Lock></button>
             <button className="work-button">Module 20 <Lock></Lock></button>
         </div>
@@ -154,12 +182,12 @@ function Work(){
         <>
             {module.length==0 ?
             <div className="Work-button-container">
+            <button className="work-button" onClick={setModuleCat}>Program Project</button>
             <button className="work-button" onClick={setModuleCat}>Module 12</button>
             <button className="work-button" onClick={setModuleCat}>Module 14</button>
             <button className="work-button" onClick={setModuleCat}>Module 15</button>
             <button className="work-button" onClick={setModuleCat}>Module 16</button>
-            <button className="work-button">Module 17 <Lock></Lock></button>
-            <button className="work-button">Module 18 <Lock></Lock></button>
+            <button className="work-button" onClick={setModuleCat}>Module 17</button>
             <button className="work-button">Module 19 <Lock></Lock></button>
             <button className="work-button">Module 20 <Lock></Lock></button>
             </div>
